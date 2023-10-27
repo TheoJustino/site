7095441ed686e84cc32da89fa4bb1dc1db2614de
TheoJustino <126608679+TheoJustino@users.noreply.github.com>
2023-10-26 21:41:04 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# TheoJustino.github.io
---
+++ Script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded"), function(){
+
+    const CalcularButton = document.getElementById("calcular");
+    const ResultadoElement = document.getElementById("resultado"); 
+
+    CalcularButton.addEventListener("click"), function () {
+
+        
+
+        ResultadoElement.textContent = 
+
+    }
+
+    function {}
+}
